7ca1288f4a32d60e9309c8ffd4c96ec34435dbae
KBajrami <78493104+KBajrami@users.noreply.github.com>
2021-10-28 03:29:11 -0400

    Add files via upload

--- webproduction2/script.js
+++ webproduction2/script.js
@@ -5,7 +5,12 @@ $(function() {
       if (agentName !== '') {
         $.getJSON(`https://valorant-api.com/v1/agents/${agentName}`, function(data) {
           console.log('data: ', data)
-          $("#agent").attr({src: data.data.bustPortrait})
+          $("#agent").attr({src: data.data.displayIcon})
+           $("#name").text(data.data.displayName)
+            $("#background").text(data.data.description)
+              $("#description").text(data.data.role.displayName)
+
+
         }).fail(function() {
           console.log("that agent doesn't exist")
       })
